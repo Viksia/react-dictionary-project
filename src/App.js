7fafd10eb3323e92b78379393e4c77cca8875b31
Viksia <105889022+Viksia@users.noreply.github.com>
2022-09-24 19:37:06 +0200

    Started to create a Dictionary App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <h1>My Dictionary App on React!</h1>
+    </div>
+  );
+}
