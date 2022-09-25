d8d7ea3ed54f20f0a117c911d12d2e5c4f861b7d
Viksia <105889022+Viksia@users.noreply.github.com>
2022-09-25 19:15:54 +0200

    Added Search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App-header">
-      <img src={logo} className="App-logo img-fluid" alt="logo" />
-      <h1>My Dictionary App on React!</h1>
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>My Dictionary App on React!</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Viksy</footer>
+      </div>
     </div>
   );
 }
