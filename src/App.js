d7cafdba7487def53ee839902c4b1fbb809d4629
Viktoriia Boiko <boiko.viktorika@gmail.com>
2023-03-19 17:47:44 +0100

    Changed API for images

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
-        <main>
-          <Dictionary defaultKeyword="yoga" />
-        </main>
-        <footer className="App-footer">
-          <a
-            href="https://github.com/Viksia/react-dictionary-project"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Coded by Viksy
-          </a>
-        </footer>
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<header className="App-header">
+					<img src={logo} className="App-logo img-fluid" alt="logo" />
+				</header>
+				<main>
+					<Dictionary defaultKeyword="sunset" />
+				</main>
+				<footer className="App-footer">
+					<a
+						href="https://github.com/Viksia/react-dictionary-project"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Coded by Viksy
+					</a>
+				</footer>
+			</div>
+		</div>
+	);
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,67 +5,66 @@ import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState(props.defaultKeyword);
-  let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false);
-  let [photos, setPhotos] = useState(null);
+	let [keyword, setKeyword] = useState(props.defaultKeyword);
+	let [results, setResults] = useState(null);
+	let [loaded, setLoaded] = useState(false);
+	let [photos, setPhotos] = useState(null);
 
-  function handleDictionResponse(response) {
-    setResults(response.data[0]);
-  }
+	function handleDictionResponse(response) {
+		setResults(response.data[0]);
+	}
 
-  function handlePexelsResponse(response) {
-    setPhotos(response.data.photos);
-  }
+	function handlePexelsResponse(response) {
+		setPhotos(response.data.photos);
+	}
 
-  function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleDictionResponse);
+	function search() {
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleDictionResponse);
 
-    let pexelsApiKey =
-      "563492ad6f91700001000001f2ddcd4d8c2040a1a5a92d54eeab8675";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let header = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: header }).then(handlePexelsResponse);
-  }
+		let pexelsApiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+		let pexelsApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${pexelsApiKey}`;
+		let header = { Authorization: `Bearer ${pexelsApiKey}` };
+		axios.get(pexelsApiUrl, { headers: header }).then(handlePexelsResponse);
+	}
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
+	function handleSubmit(event) {
+		event.preventDefault();
+		search();
+	}
 
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
-  }
+	function handleKeywordChange(event) {
+		setKeyword(event.target.value);
+	}
 
-  function load() {
-    setLoaded(true);
-    search();
-  }
+	function load() {
+		setLoaded(true);
+		search();
+	}
 
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <section>
-          <h1>What word do you want to look up?</h1>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="search"
-              onChange={handleKeywordChange}
-              defaultValue={props.defaultKeyword}
-            />
-          </form>
-          <div className="hint">
-            <strong>Suggested words:</strong> sunset, wine, sunrise, forest,
-            plants
-          </div>
-        </section>
-        <Results results={results} />
-        <Photos photos={photos} />
-      </div>
-    );
-  } else {
-    load();
-    return "Loading..";
-  }
+	if (loaded) {
+		return (
+			<div className="Dictionary">
+				<section>
+					<h1>What word do you want to look up?</h1>
+					<form onSubmit={handleSubmit}>
+						<input
+							type="search"
+							onChange={handleKeywordChange}
+							defaultValue={props.defaultKeyword}
+						/>
+					</form>
+					<div className="hint">
+						<strong>Suggested words:</strong> sunset, wine, sunrise, forest,
+						plants
+					</div>
+				</section>
+				<Results results={results} />
+				<Photos photos={photos} />
+			</div>
+		);
+	} else {
+		load();
+		return "Loading..";
+	}
 }
